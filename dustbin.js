df90f6332f83e93271871e989467456d994bd474
tejashvini10 <74137391+tejashvini10@users.noreply.github.com>
2021-01-24 11:31:50 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,58 @@
+class Dustbin {
+    constructor(x,y){
+       
+        this.x=x;
+		this.y=y;
+          this.width=200;
+        this.height=220;
+        this.wallThickness=20;
+        this.image=loadImage("dustbin.png")
+        this.bottomBody=Bodies.rectangle(this.x, this.y, this.width, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.width/2, this.y-this.height/2, this.wallThickness, this.height, {isStatic:true})
+		
+		this.rightWallBody=Bodies.rectangle(this.x+this.width/2, this.y-this.height/2, this.wallThickness, this.height, {isStatic:true})
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+      
+ 
+   
+    }
+    display()
+    {
+        var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+        
+  
+           push()
+           translate(posLeft.x, posLeft.y);
+           angleMode(RADIANS)
+           fill(255)
+           rotate(this.angle)
+           rectMode(CENTER);
+  
+            pop()
+    
+    push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			
+			pop()
+    
+    push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			
+			angleMode(RADIANS)
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.height/2,this.width, this.height)
+			pop()
+}
+}
---
+++ sketch.js
@@ -0,0 +1,62 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render=Matter.Render;
+
+var engine,world;
+var paper,ground,dustbinImage;
+var dustbin;
+
+
+
+
+function setup() {
+	createCanvas(1500,900);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+paper=new Paper(200,450,70);
+ground=new Ground(width/2,660,width,20);
+
+dustbin=new Dustbin(1200,650);
+	
+
+	var render = Render.create({
+	element: document.body,
+	engine: engine,
+	options: {
+	  width: 1500,
+	  height: 700,
+	  wireframes: false
+	}
+});
+
+Engine.run(engine);
+Render.run(render);
+
+
+  
+}
+
+
+
+
+function draw() {
+ 
+  background(230);
+  rectMode(CENTER);
+  paper.display();
+  ground.display();
+  dustbin.display();
+  
+
+ 
+}
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:130,y:-145});
+	}
+}
